61278665cf9b060dd13e6e5334d52c2f61b6adee
OwOskan <42584725+Lex0planet@users.noreply.github.com>
2025-12-02 11:24:34 +0100

    Tile System generates a simple circle

---
+++ Assets/Scripts/TileSystem/TileSystem.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSystem : MonoBehaviour
+{
+    [Header("Map size")]
+    [SerializeField] int xSize = 50;
+    [SerializeField] int ySize = 50;
+    
+    [SerializeField]List<Tile> tiles = new();
+
+
+    [Header("Island")] 
+    [SerializeField] int yCentre = 25;
+    [SerializeField] int xCentre = 25;
+    [SerializeField] int radius = 20;
+    [SerializeField] List<int> edgePoints = new();
+    
+    void Awake()
+    {
+        for (int i = 0; i < 360; i++)
+        {
+            float angle = i * Mathf.Deg2Rad;
+            float x1 = xCentre + radius * Mathf.Cos(angle);
+            float y1 = yCentre + radius * Mathf.Sin(angle);
+            int calcIndex = Mathf.RoundToInt(y1) * xSize + Mathf.RoundToInt(x1);
+            edgePoints.Add(calcIndex);
+        }
+        
+        for (int x = 0; x < xSize * ySize; x++)
+        {
+            if (edgePoints.Contains(x))
+                tiles.Add(new Tile(3));
+            else
+                tiles.Add(new Tile());
+        }
+
+        GetTile(new Vector2Int(10, 15)).level = 3;
+    }
+
+    void Start()
+    {
+        Debug.Log(GetTile(new Vector2Int(10, 15)).level);
+        Debug.Log(Mathf.PerlinNoise(GetPositionFromIndex(760).x, GetPositionFromIndex(760).y)*10);
+    }
+    
+    Tile GetTile(Vector2Int tilePos)
+    {
+        int calcIndex = tilePos.y * xSize + tilePos.x;
+        return tiles[calcIndex];
+    }
+
+    Vector2Int GetPositionFromIndex(int index)
+    {
+        return new Vector2Int(index % xSize, index / xSize);
+    }
+
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < tiles.Count; i++)
+        {
+            Gizmos.color = Color.yellow;
+            if (tiles[i].level == 3)
+                Gizmos.color = Color.red;
+            
+            Vector2Int tilePos = GetPositionFromIndex(i);
+            
+            Gizmos.DrawCube(new Vector3(tilePos.x, 0, tilePos.y), Vector3.one);
+        }  
+    }
+}
+
+[System.Serializable]
+public class Tile
+{
+    public int level = 0;
+
+    public Tile( int level=0)
+    {
+        this.level = level;
+    }
+    
+}
