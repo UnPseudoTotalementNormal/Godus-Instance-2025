17b034851f4d10556237f6021522055a9d2d3617
OwOskan <42584725+Lex0planet@users.noreply.github.com>
2025-12-05 11:08:44 +0100

    Added an efficient way for AI to request a task

---
+++ Assets/Scripts/AI/BehaviorTrees/CustomActions/AskForTaskAction.cs
@@ -0,0 +1,27 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Ask for Task", story: "Ask [VillageManager] for a task", category: "Action", id: "51e0926e754fd4c45642012fd93a6e6e")]
+public partial class AskForTaskAction : Action
+{
+    [SerializeReference] public BlackboardVariable<VillageManager> VillageManager;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
--- Assets/Scripts/AI/VillageManager.cs
+++ Assets/Scripts/AI/VillageManager.cs
@@ -1,21 +1,30 @@
 using Unity.Behavior;
-using Unity.Behavior.GraphFramework;
 using UnityEngine;
 
 public class VillageManager : MonoBehaviour
 {
-    TaskManager taskManager;
-    ResourceManager resourceManager;
-
     [SerializeField] BehaviorGraphAgent villageBlackboard;
     [SerializeField] Vector2Int villageCenter;
 
     bool villageUnderAttack;
+    
+    //Resources
+    int wood;
+    int maxWood = 50;
+    int stone;
+    int maxStone = 50;
+    int iron;
+    int maxIron = 50;
+    int glorp;
+    int maxGlorp = 50;
+    int meat;
+    int maxMeat = 50;
 
     void Start()
     {
         Debug.Log(villageBlackboard.GetVariable("RollCallActive", out var active));
         villageBlackboard.SetVariableValue("VillageCenter", villageCenter);
+        villageBlackboard.SetVariableValue("VillageManager", this);
     }
     
     [ContextMenu("Roll call")]
@@ -24,4 +33,48 @@ public class VillageManager : MonoBehaviour
         villageUnderAttack = !villageUnderAttack;
         villageBlackboard.SetVariableValue("RollCallActive", villageUnderAttack);
     }
+
+    public TaskType GetNewTask(out GameObject _target)
+    {
+        _target = null; // Remove when all functions have been created
+        if ((100 * (wood / maxWood)) >= 95)
+        {
+            return TaskType.Building;
+        }
+        
+        if ((100 * (meat / maxMeat)) <= 80)
+        {
+            return TaskType.Hunting;
+        }
+
+        if ((100 * (wood / maxWood)) <= 80)
+        {
+            return TaskType.Gathering;
+        }
+
+        if ((100 * (stone / maxStone)) <= 50)
+        {
+            return TaskType.Gathering;
+        }
+
+        if ((100 * (iron / maxIron)) <= 50)
+        {
+            return TaskType.Gathering;
+        }
+
+        if ((100 * (glorp / maxGlorp)) <= 50)
+        {
+            return TaskType.Gathering;
+        }
+
+        return TaskType.Wandering;
+    }
+}
+
+public enum TaskType
+{
+    Gathering,
+    Building,
+    Hunting,
+    Wandering,
 }
