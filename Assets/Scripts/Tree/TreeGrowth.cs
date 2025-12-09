800bd873ec45018f1ee6939628183bedc7780d26
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-12-09 14:04:33 +0100

    Tree now growth !

---
+++ Assets/Scripts/Tree/TreeGrowth.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TreeGrowth : MonoBehaviour
+{
+    [Header("Paramètres de croissance")]
+    public float growthDuration = 90f;                   // Temps total de croissance
+    public Vector2 finalScale = new(2f, 2f); // Taille finale
+
+    private Vector2 initialScale;
+    private float growthTimer = 0f;
+    private bool canBeCollected = false;
+
+    void Start()
+    {
+        initialScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        if (growthTimer < growthDuration)
+        {
+            growthTimer += Time.deltaTime;
+
+            float _t = growthTimer / growthDuration;
+
+            transform.localScale = Vector2.Lerp(initialScale, finalScale, _t);
+        }
+        else
+        {
+            canBeCollected = true;
+        }
+    }
+}
