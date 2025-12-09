3e20bb8c4e758d1ac3bf1e71b02362c1b2fcbc21
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2025-12-09 10:36:43 +0100

    Adding AttackComponent and HealthComponent

---
+++ Assets/Scripts/AI/AttackComponent.cs
@@ -0,0 +1,54 @@
+using System;
+using TileSystemSpace;
+using UnityEngine;
+
+namespace AI
+{
+    public class AttackComponent : MonoBehaviour
+    {
+        [SerializeField] private float attackRange = 1.5f;
+        [SerializeField] private float attackDamage = 10f;
+        [SerializeField] private float attackCooldown = 1f;
+        [SerializeField] private float attackKnockbackForce = 1f;
+        
+        private float lastAttackTime = -Mathf.Infinity;
+        
+        public event Action onAttack;
+        
+        public virtual bool IsInAttackRange(Vector2 _target)
+        {
+            float _distanceToTarget = Vector2.Distance(transform.position, _target);
+            return _distanceToTarget <= attackRange;
+        }
+        public bool IsInAttackRange(HealthComponent _target) => IsInAttackRange(_target.transform.position);
+        public bool IsInAttackRange(Transform _target) => IsInAttackRange(_target.transform.position);
+
+        public bool TryAttack(HealthComponent _targetHealth)
+        {
+            if (!IsInAttackRange(_targetHealth))
+            {
+                return false;
+            }
+            
+            if (Time.time < lastAttackTime + attackCooldown)
+            {
+                return false;
+            }
+            
+            Attack(_targetHealth);
+            return true;
+        }
+        
+        public void ResetAttackCooldown()
+        {
+            lastAttackTime = -Mathf.Infinity;
+        }
+        
+        private void Attack(HealthComponent _targetHealth)
+        {
+            _targetHealth.TakeDamage(attackDamage);
+            lastAttackTime = Time.time;
+            onAttack?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/AI/HealthComponent.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
+    
+    public float GetCurrentHealth() => currentHealth;
+    public float GetMaxHealth() => maxHealth;
+
+    private event Action<float> onDamaged;
+    private event Action<float> onHealed;
+    private event Action onDeath;
+    
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth -= _damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        onDamaged?.Invoke(_damage);
+    }
+    
+    public void Heal(float _healAmount)
+    {
+        currentHealth += _healAmount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        onHealed?.Invoke(_healAmount);
+    }
+
+    private void Die()
+    {
+        onDeath?.Invoke();
+        Destroy(gameObject);
+    }
+}
