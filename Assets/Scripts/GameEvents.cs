d8529d5c2844cbeabcec4a97c1d51817d903a63d
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-12-09 09:33:57 +0100

    SpawnEnemy + fix EnemyWave

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -4,4 +4,9 @@ public static class GameEvents
 {
     public static Action onWaveStarted;
     public static Action onWaveEnded;
+    public static Action onEnemyDeath;
+    public static Action<WaveInfo> onWaveInfo;
+    public static Action<bool> onEnabledSlideBarRemainingEnemy;
+    public static Action onEnableSlideBarRemainingEnemy;
+    public static Action onDisableSlideBarRemainingEnemy;
 }
