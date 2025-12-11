530155574b790446e187eff0db93ccd40951ea81
LambdaCrea <breyn.dumas@gmail.com>
2025-12-11 16:38:24 +0100

    PR_MainMenu
    Contient :
    - MainMenuScene
    - MainMenuScript
    - CreditsScript

---
+++ Assets/Scripts/UI/MainMenu/CreditsScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour
+{
+
+    public void Malena()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Karl()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Lucas()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+    public void Axel()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Gabriel()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Nathan()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Josselyn()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Dorian()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Maxence()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Breynann()
+    {
+        Application.OpenURL("https://www.google.com/");
+    }
+
+    public void Glorpy()
+    {
+        Application.OpenURL("https://c.tenor.com/4Od0NUWfA54AAAAd/tenor.gif");
+    }
+}
