618c0fcee6577f8e03f57522aff6ca1ff0ca6b23
OwOskan <42584725+Lex0planet@users.noreply.github.com>
2025-12-15 17:07:11 +0100

    Updated upgrade requirements

--- Assets/Scripts/AI/VillageManager.cs
+++ Assets/Scripts/AI/VillageManager.cs
@@ -122,7 +122,7 @@ public class VillageManager : MonoBehaviour
             }
         }
 
-        if (Random.value <= 0.6f)
+        if (Random.value <= 0.6f && (villageData.glorp >= 10 || villageData.iron >= 10 || villageData.stone >= 10))
         {
             Debug.Log(_caller.name + "wants to go shop !");
             //Make the AI go back to the village centre to upgrade
