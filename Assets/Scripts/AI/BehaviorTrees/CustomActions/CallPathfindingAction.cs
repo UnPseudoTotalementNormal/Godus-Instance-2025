ffaad9747dbde846bd66e45536bbc59d4061a049
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2025-12-15 14:58:42 +0100

    Fixing pathfinding new()

--- Assets/Scripts/AI/BehaviorTrees/CustomActions/CallPathfindingAction.cs
+++ Assets/Scripts/AI/BehaviorTrees/CustomActions/CallPathfindingAction.cs
@@ -20,8 +20,11 @@ public partial class CallPathfindingAction : Action
     
     protected override Status OnStart()
     {
-        pathfinder = new Pathfinding();
-        pathfinder.callback += PathfindingCallback;
+        if (pathfinder == null)
+        {
+            pathfinder = new Pathfinding();
+            pathfinder.callback += PathfindingCallback;
+        }
         pathfinder.FindPath(new Vector2Int((int)Agent.Value.transform.position.x, (int)Agent.Value.transform.position.y), new Vector2Int((int)Target.Value.transform.position.x, (int)Target.Value.transform.position.y));
         return Status.Running;
     }
