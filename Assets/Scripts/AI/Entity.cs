a4c4429e781c0cd1716dd02cf49b42bb2c2e3cfd
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2025-12-15 11:10:07 +0100

    Fix entity script

--- Assets/Scripts/AI/Entity.cs
+++ Assets/Scripts/AI/Entity.cs
@@ -1,8 +1,18 @@
+using System;
 using AI;
 using UnityEngine;
 
 public class Entity : MonoBehaviour, ITeamComponent
 {
     [field:SerializeField] public EntityTeam team { get; private set; }
+
+    public event Action onDeath;
+    
+    [ContextMenu("DIE")]
+    public void Die()
+    {
+        onDeath?.Invoke();
+        Destroy(gameObject);
+    }
 }
 
