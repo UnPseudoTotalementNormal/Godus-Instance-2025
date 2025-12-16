6bc471417fbb9f0124237062c89f4f9a206a11a3
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-12-16 19:49:43 +0100

    SetWaves + fix main menu settings

--- Assets/Scripts/UI/MainMenu/CreditsScript.cs
+++ Assets/Scripts/UI/MainMenu/CreditsScript.cs
@@ -1,62 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class CreditsScript : MonoBehaviour
+public class CreditsScript : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private string link;
 
-    public void Malena()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Karl()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Lucas()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-    public void Axel()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Gabriel()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Nathan()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Josselyn()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Dorian()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Maxence()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Breynann()
-    {
-        Application.OpenURL("https://www.google.com/");
-    }
-
-    public void Glorpy()
-    {
-        Application.OpenURL("https://c.tenor.com/4Od0NUWfA54AAAAd/tenor.gif");
+        Application.OpenURL(link);
     }
 }
