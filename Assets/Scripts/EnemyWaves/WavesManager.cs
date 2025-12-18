012a190bb5996b9bf7fe337f9093a8569a344c28
PandorTaxe <beaumont.nathan0112@gmail.com>
2025-12-18 01:51:44 +0100

    Fix spawn waves manager that make while true

--- Assets/Scripts/EnemyWaves/WavesManager.cs
+++ Assets/Scripts/EnemyWaves/WavesManager.cs
@@ -214,6 +214,10 @@ public class WavesManager : MonoBehaviour
 
     private void SetWaveSpawnPoint()
     {
+        const int _maxTriesBeforeIgnoringEnemies = 1000;
+        int _tries = 0;
+        bool _valid;
+
         do
         {
             waveSpawnPoint = new Vector3(
@@ -227,8 +231,13 @@ public class WavesManager : MonoBehaviour
                 Mathf.RoundToInt(waveSpawnPoint.y)
             );
 
-        }
-        while (_tileForSpawn.tileType == TileType.Water || IsEnemyNextTo());
+            bool _isWater = _tileForSpawn.tileType == TileType.Water;
+            bool _enemyNearby = _tries < _maxTriesBeforeIgnoringEnemies && IsEnemyNextTo();
+
+            _valid = !_isWater && !_enemyNearby;
+            _tries++;
+
+        } while (!_valid);
     }
 
     private IEnumerator SpawnGroupCoroutine(EnemyGroup _group)
